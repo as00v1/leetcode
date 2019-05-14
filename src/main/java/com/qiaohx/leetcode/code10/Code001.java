75b471f10e85a1a131ece2ee32f485bea7269422
jiayong <36148963@qq.com>
2019-05-14 09:35:38 +0800

    1-5题

--- src/main/java/com/qiaohx/leetcode/code10/Code001.java
+++ src/main/java/com/qiaohx/leetcode/code10/Code001.java
@@ -1,5 +1,8 @@
 package com.qiaohx.leetcode.code10;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Copyright (c) 2019, jiayong. All rights reserved.
  *
@@ -9,17 +12,13 @@ package com.qiaohx.leetcode.code10;
  */
 public class Code001 {
 
-    /**
-     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
-     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
-     * https://leetcode-cn.com/problems/two-sum/
-     * @param args
-     */
 	public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
+        int[] nums = {3, 5, 11, 21};
         int target = 26;
-
-        int[] res = sum(nums, target);
+        long start = System.currentTimeMillis();
+        int[] res = sum3(nums, target);
+        long end = System.currentTimeMillis();
+        System.out.println("耗时" + (end - start) + "ms");
         if (res != null) {
             for (int var : res) {
                 System.out.print(var + ",");
@@ -29,7 +28,13 @@ public class Code001 {
         }
         
     }
-
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+     * https://leetcode-cn.com/problems/two-sum/
+     * @param nums 整数数组
+     * @param target 目标值
+     */
     public static int[] sum(int nums[], int target){
         int i, j;
         for (i = 0; i < nums.length - 1; i++) {
@@ -40,6 +45,44 @@ public class Code001 {
                     return new int[]{i ,j};
                 }
             }
+        }
+        return null;
+    }
+
+
+    /**
+     * 第二种解法 利用hashMap
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] sum2(int nums[], int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);// 将数组构建哈希表
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];// 计算出要找到的数字
+            if (map.containsKey(num) && map.get(num) != i)// 看下在不在map里面
+                return new int[]{i, map.get(num)};
+        }
+        return null;
+    }
+
+    /**
+     * 基于第二种优化
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] sum3(int nums[], int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 构建的时候就可以检查了
+            int num = target - nums[i];// 计算出要找到的数字
+            if (map.containsKey(num) && map.get(num) != i)// 看下在不在map里面
+                return new int[]{map.get(num), i};
+            map.put(nums[i], i);// 将数组构建哈希表
         }
         return null;
     }
